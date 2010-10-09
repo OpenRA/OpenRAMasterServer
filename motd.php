21bcd7d2770ec0ca7c36e9c45d9ef45016c1e0c5
Matthew Bowra-Dean <matthew@ijw.co.nz>
2010-10-09 23:11:11 +1300

    Fixed MOTD script for minor releases.

--- motd.php
+++ motd.php
@@ -1,14 +1,31 @@
-<?
+<?php
         $inVer = substr($_GET["v"], strpos($_GET["v"], "-")+1);
+
+        $inVer_min = 1;
+        $i = strpos($inVer, "-");
+        if ($i !== false)
+        {
+            $inVer_min = substr($inVer, $i+1);
+            $inVer = substr($inVer, 0, $i);
+        }
+
         $verFile = "VERSION";
         $handle = fopen($verFile, 'r');
 
         $version = fread($handle, filesize($verFile));
         fclose($handle);
 
-        $version = substr($version, strpos($version, "-")+1);
-        
-        if ($inVer < $version)
+        $version = trim(substr($version, strpos($version, "-")+1));
+        $version_min = 1;
+
+        $i = strpos($version, "-");
+        if ($i !== false)
+        {
+            $version_min = trim(substr($version, $i+1));
+            $version = substr($version, 0, $i);
+        }
+
+        if ($inVer < $version || ($inVer == $version && $inVer_min < $version_min))
         {
                 echo "New version available. Please go to http://open-ra.org to upgrade";
         }
