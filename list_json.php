187dd9432b7a40f4980baa00c5116b1f83af1529
Nathan Sullivan <nathan@nightsys.net>
2011-07-15 08:13:42 +1000

    adding JSON version of list, inspired by _aegis_

---
+++ list_json.php
@@ -0,0 +1,33 @@
+<?php
+
+#header('Content-Type: text/plain');
+
+    try
+    {
+        $db = new PDO('sqlite:db/openra.db');
+        $stale = 60 * 5;
+        $result = $db->query('SELECT * FROM servers WHERE (' . time() . ' - ts < ' . $stale . ') ORDER BY name');
+	$n = 0;
+	$json_result_array = array();
+        foreach ( $result as $row )
+        {
+	    $game_result = array();
+	    $game_result['map'] = $row['map'];
+	    $game_result['mods'] = $row['mods'];
+	    $game_result['name'] = $row['name'];
+	    $game_result['ttl'] = ($stale - (time() - $row['ts']));
+	    $game_result['players'] = $row['players'];
+	    $game_result['state'] = $row['state'];
+	    $game_result['address'] = $row['address'];
+	    $game_result['id'] = $row['id'];
+	    $json_result_array[] = $game_result;
+	    unset($game_result);
+	}
+	print(json_encode($json_result_array));
+        $db = null;
+    }
+    catch (PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+?>
