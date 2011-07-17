46b43d902083e41b3f8f0022b8aae6bfb39b9883
Matthew Bowra-Dean <matthew@ijw.co.nz>
2011-07-17 22:53:46 +1200

    Fix indentation.

--- list_json.php
+++ list_json.php
@@ -7,23 +7,23 @@ header('Content-Type: application/javascript');
         $db = new PDO('sqlite:db/openra.db');
         $stale = 60 * 5;
         $result = $db->query('SELECT * FROM servers WHERE (' . time() . ' - ts < ' . $stale . ') ORDER BY name');
-	$n = 0;
-	$json_result_array = array();
+        $n = 0;
+        $json_result_array = array();
         foreach ( $result as $row )
         {
-	    $game_result = array();
-	    $game_result['map'] = $row['map'];
-	    $game_result['mods'] = $row['mods'];
-	    $game_result['name'] = $row['name'];
-	    $game_result['ttl'] = ($stale - (time() - $row['ts']));
-	    $game_result['players'] = $row['players'];
-	    $game_result['state'] = $row['state'];
-	    $game_result['address'] = $row['address'];
-	    $game_result['id'] = $row['id'];
-	    $json_result_array[] = $game_result;
-	    unset($game_result);
-	}
-	print(json_encode($json_result_array));
+            $game_result = array();
+            $game_result['map'] = $row['map'];
+            $game_result['mods'] = $row['mods'];
+            $game_result['name'] = $row['name'];
+            $game_result['ttl'] = ($stale - (time() - $row['ts']));
+            $game_result['players'] = $row['players'];
+            $game_result['state'] = $row['state'];
+            $game_result['address'] = $row['address'];
+            $game_result['id'] = $row['id'];
+            $json_result_array[] = $game_result;
+            unset($game_result);
+        }
+        print(json_encode($json_result_array));
         $db = null;
     }
     catch (PDOException $e)
