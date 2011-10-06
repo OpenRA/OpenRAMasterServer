8b73d05be018eb8c2f63bd57a57f6b9170d6c7bb
Igor Popov <ihptru@gmail.com>
2011-10-06 21:32:23 +0400

    Everyone should not be able to DROP/CREATE again Master Server Database over remote web page access

--- create.php
+++ create.php
@@ -3,13 +3,11 @@
     try
     {
         $db = new PDO('sqlite:db/openra.db');
-        echo 'Connection to DB established.\n';
-        if ($db->query('DROP TABLE servers'))
-            echo 'Dropped table.\n';
-        $schema = 'CREATE TABLE servers (id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(255), 
+        echo "Connection to DB established.\n";
+        $schema = 'CREATE TABLE IF NOT EXISTS servers (id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(255), 
             address varchar(255) UNIQUE, players integer, state integer, ts integer, map varchar(255), mods varchar(255))';
-        if ($db->query($schema))
-            echo 'Created table.';
+        $db->query($schema);
+		echo "Connection to DB closed.\n";
         $db = null;
     }
     catch (PDOException $e)
