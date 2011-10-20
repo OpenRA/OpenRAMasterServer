a394d60b78d433fefe6f01026b5a529076cb1a7c
Igor Popov <ihptru@gmail.com>
2011-10-20 22:47:07 +0400

    Extended Master Server: map data + players

--- ping.php
+++ ping.php
@@ -14,39 +14,78 @@
     if (!isset( $_REQUEST['port'] )) exit();
     if (!isset( $_REQUEST['name'] )) exit();
     if (!isset( $_REQUEST['state'] )) exit();
-    if (!isset( $_REQUEST['map'] )) exit();
-    if (!isset( $_REQUEST['mods'] )) exit();
     if (!isset( $_REQUEST['players'] )) exit();
-
+    if (!isset( $_REQUEST['mods'] )) exit();
+    if (!isset( $_REQUEST['map'] )) exit();
+    if (!isset( $_REQUEST['maxplayers'] )) exit();
+    if (!isset( $_REQUEST['title'] )) exit();
+    if (!isset( $_REQUEST['description'] )) exit();
+    if (!isset( $_REQUEST['type'] )) exit();
+    if (!isset( $_REQUEST['width'] )) exit();
+    if (!isset( $_REQUEST['height'] )) exit();
+    if (!isset( $_REQUEST['tileset'] )) exit();
+    if (!isset( $_REQUEST['author'] )) exit();
+    
     header( 'Content-type: text/plain' );
     try 
     {
         $db = new PDO('sqlite:db/openra.db');
+        
         $ip = $_SERVER['REMOTE_ADDR'];
         $port = $_REQUEST['port'];
         $addr = $ip . ':' . $port;
         $name = urldecode( $_REQUEST['name'] );
+        $state = $_REQUEST['state'];
+        $players = (int)$_REQUEST['players'];
         
 		if (isset( $_REQUEST['new']))
 		{
             $connectable = check_port($ip, $port);
             if (!$connectable)
                 $name = '[down]' . $name;
+                $state = -1;
 		}
-        
+		
         $insert = $db->prepare('INSERT OR REPLACE INTO servers 
-            (name, address, players, state, ts, map, mods) 
-            VALUES (:name, :addr, :players, :state, :time, :map, :mods)');
+            (name, address, players, state, ts, map, mods, maxplayers, title, description, type, width, height, tileset, author) 
+            VALUES (:name, :addr, :players, :state, :time, :map, :mods, :maxplayers, :title, :description, :type, :width, :height, :tileset, :author)');
         $insert->bindValue(':name', $name, PDO::PARAM_STR);
         $insert->bindValue(':addr', $addr, PDO::PARAM_STR);
-        $insert->bindValue(':players', $_REQUEST['players'], PDO::PARAM_INT);
-        $insert->bindValue(':state', $_REQUEST['state'], PDO::PARAM_INT);
+        $insert->bindValue(':players', $players, PDO::PARAM_INT);
+        $insert->bindValue(':state', $state, PDO::PARAM_INT);
         $insert->bindValue(':time', time(), PDO::PARAM_INT);
         $insert->bindValue(':map', $_REQUEST['map'], PDO::PARAM_STR);
         $insert->bindValue(':mods', $_REQUEST['mods'], PDO::PARAM_STR);
-        
+        $insert->bindValue(':maxplayers', $_REQUEST['maxplayers'], PDO::PARAM_INT);
+        $insert->bindValue(':title', $_REQUEST['title'], PDO::PARAM_STR);
+        $insert->bindValue(':description', $_REQUEST['description'], PDO::PARAM_STR);
+        $insert->bindValue(':type', $_REQUEST['type'], PDO::PARAM_STR);
+        $insert->bindValue(':width', $_REQUEST['width'], PDO::PARAM_INT);
+        $insert->bindValue(':height', $_REQUEST['height'], PDO::PARAM_INT);
+        $insert->bindValue(':tileset', $_REQUEST['tileset'], PDO::PARAM_STR);
+        $insert->bindValue(':author', $_REQUEST['author'], PDO::PARAM_STR);
         $insert->execute();
-
+        
+        $delete = $db->prepare("DELETE FROM players WHERE server_address = '" . $addr . "'");
+        $delete->execute();
+        
+        $i = 0;
+        while ($i < $players)
+        {
+			$playerName = $_REQUEST['playerName'.(string)$i];
+			$playerFaction = $_REQUEST['playerFaction'.(string)$i];
+			$playerTeam = $_REQUEST['playerTeam'.(string)$i];
+			$insert = $db->prepare('INSERT INTO players
+            (server_address, name, faction, team) 
+            VALUES (:server_address, :name, :faction, :team)');
+            $insert->bindValue(':server_address', $addr, PDO::PARAM_STR);
+            $insert->bindValue(':name', $playerName, PDO::PARAM_STR);
+            $insert->bindValue(':faction', $playerFaction, PDO::PARAM_STR);
+            $insert->bindValue(':team', $playerTeam, PDO::PARAM_INT);
+            $insert->execute();
+            $i++;
+		}
+		
         if (isset( $_REQUEST['new']))
         {
             $select = $db->prepare('SELECT id FROM servers WHERE address = :addr');
