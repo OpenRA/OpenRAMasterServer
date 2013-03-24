f9f2eda8ebd5f63b4e5febed169bd68b1954951c
xaionaro <xai@mephi.ru>
2013-03-24 19:31:00 +0400

    Fix of issue #4

--- ping.php
+++ ping.php
@@ -1,78 +1,96 @@
 <?php
-    define('PORT_CHECK_TIMEOUT', 3);
-    
-    function check_port($ip, $port)
-    {
-        $sock = @fsockopen($ip, $port, $errno, $errstr, PORT_CHECK_TIMEOUT);
-        if (!$sock)
-            return false;
-        fclose($sock);
-        return true;
-    }
+	// === configuration ===
 
-    // make sure everything required is actually set.
-    if (!isset( $_REQUEST['port'] )) exit();
-    if (!isset( $_REQUEST['name'] )) exit();
-    if (!isset( $_REQUEST['state'] )) exit();
-    if (!isset( $_REQUEST['map'] )) exit();
-    if (!isset( $_REQUEST['mods'] )) exit();
-    if (!isset( $_REQUEST['players'] )) exit();
+	define('DEBUG',				1);
+	define('PORT_CHECK_TIMEOUT',		3);
 
-    header( 'Content-type: text/plain' );
-    try 
-    {
-        $db = new PDO('sqlite:db/openra.db');
-        $ip = $_SERVER['REMOTE_ADDR'];
-        $port = $_REQUEST['port'];
-        $addr = $ip . ':' . $port;
-        $name = urldecode( $_REQUEST['name'] );
-        
-        if ($_REQUEST['state'] == 3)
-        {
-               $remove = $db->prepare('DELETE FROM servers WHERE address = :addr');
-               $remove->bindValue(':addr', $addr, PDO::PARAM_STR);
-               $remove->execute();
-               $db = null;
-               exit;
-        }
+	ini_set('display_errors', 		DEBUG);
 
-        if (isset( $_REQUEST['new']))
-        {
-            $connectable = check_port($ip, $port);
-            if (!$connectable)
-                $name = '[down]' . $name;
-        }
+	error_reporting(DEBUG ? E_ALL : 0);
 
-        $insert = $db->prepare('INSERT OR REPLACE INTO servers 
-            (name, address, players, state, ts, map, mods) 
-            VALUES (:name, :addr, :players, :state, :time, :map, :mods)');
-        $insert->bindValue(':name', $name, PDO::PARAM_STR);
-        $insert->bindValue(':addr', $addr, PDO::PARAM_STR);
-        $insert->bindValue(':players', $_REQUEST['players'], PDO::PARAM_INT);
-        $insert->bindValue(':state', $_REQUEST['state'], PDO::PARAM_INT);
-        $insert->bindValue(':time', time(), PDO::PARAM_INT);
-        $insert->bindValue(':map', $_REQUEST['map'], PDO::PARAM_STR);
-        $insert->bindValue(':mods', $_REQUEST['mods'], PDO::PARAM_STR);
-        
-        $insert->execute();
+	header('Content-type: text/plain');
 
-        if (isset( $_REQUEST['new']))
-        {
-            $select = $db->prepare('SELECT id FROM servers WHERE address = :addr');
-            $select->bindValue(':addr', $addr, PDO::PARAM_STR);
+	// === functions ===
 
-            $select->execute();
+	function die_msg($msg)
+	{
+		if (DEBUG) {
+			debug_print_backtrace();
+			die($msg);
+		}
+		die();
+	}
 
-            echo (int)$select->fetchColumn();
-    
-            $games = file_get_contents("games.txt");
-            file_put_contents("games.txt", $games + 1);
-        }
+	function check_port($ip, $port)
+	{
+		return @fsockopen($ip, $port, $errno, $errstr, PORT_CHECK_TIMEOUT);
+	}
 
-        $db = null;
-    }
-    catch (PDOException $e)
-    {
-        echo $e->getMessage();
-    }
+	function updatedbinfo($gameinfo) {
+		global $db;
+		$fields = array_keys($gameinfo);
+
+		$query  = $db->prepare('INSERT OR ABORT INTO `servers` ('.implode(', ', $fields).') VALUES (:'.implode(', :', $fields).')');
+		$result = $query->execute($gameinfo);
+		if (!$result) {
+			$query  = $db->prepare('UPDATE OR FAIL `servers` SET '.implode('=?, ', $fields).'=? WHERE address = :address');
+			$result = $query->execute(array_merge(array_values($gameinfo), array(':address' => $gameinfo['address'])));
+		}
+
+		if (DEBUG) $query->debugDumpParams();
+		return $result;
+	}
+
+	// === body ===
+
+	// make sure everything required is actually set.
+	foreach(array('port', 'name', 'state', 'map', 'mods', 'players') as $key)
+		if(!isset($_REQUEST[$key]))
+			die_msg('field "'.$key.'" is not set');
+
+	try 
+	{
+		$db = new PDO('sqlite:db/openra.db');
+
+		$ip   = $_SERVER['REMOTE_ADDR'];
+		$port = $_REQUEST['port'];
+		$addr = $ip.':'.$port;
+		$name = urldecode($_REQUEST['name']);
+		
+		if ($_REQUEST['state'] == 3)
+		{
+			$remove = $db->prepare('DELETE FROM `server` WHERE address = :addr');
+			$remove->bindValue(':addr', $addr, PDO::PARAM_STR);
+			$remove->execute();
+			unset($db);
+			exit;
+		}
+
+		if (isset($_REQUEST['new']))
+		{
+			if(!check_port($ip, $port))
+				$name = '[down]' . $name;
+
+			$games = file_get_contents("games.txt");
+			file_put_contents("games.txt", $games + 1);
+		}
+
+		updatedbinfo(
+			array(
+				'name'		=> $name,
+				'address'	=> $addr,
+				'players'	=> $_REQUEST['players'],
+				'state'		=> $_REQUEST['state'],
+				'ts'		=> time(),
+				'map'		=> $_REQUEST['map'], 
+				'mods'		=> $_REQUEST['mods'],
+			)
+		);
+		unset($db);
+	}
+	catch (PDOException $e)
+	{
+		die($e->getMessage());
+	}
+
 ?>
