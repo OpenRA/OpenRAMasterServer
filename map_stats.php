ede1f911e0a60db1a1d8708b5a421b3fb6a1eebc
ihptru <ihptru@gmail.com>
2014-07-22 22:50:16 +0400

    improved version of master server

---
+++ map_stats.php
@@ -0,0 +1,31 @@
+<?php
+    date_default_timezone_set('UTC');
+
+    if(!isset($_REQUEST['hash']))
+        die('hash argument is required.');
+
+    header('Content-Type: application/javascript');
+    header('Access-Control-Allow-Origin: *');
+
+    try
+    {
+        $db = new PDO('sqlite:db/openra.db');
+        $query = $db->prepare('SELECT played_counter FROM map_stats WHERE map = :map');
+        $query->bindValue(':map', $_REQUEST['hash'], PDO::PARAM_STR);
+        $query->execute();
+        $result = $query->fetchAll();
+        $json_result_array = array();
+        foreach ($result as $row)
+        {
+            $json_result_array['map'] = $_REQUEST['hash'];
+            $json_result_array['played'] = $row['played_counter'];
+        }
+        print(json_encode($json_result_array));
+        $db = null;
+    }
+    catch (PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+
+?>
--- ping.php
+++ ping.php
@@ -1,90 +1,210 @@
 <?php
-	// === configuration ===
+    date_default_timezone_set('UTC');
 
-	define('DEBUG',				0);
-	define('PORT_CHECK_TIMEOUT',		3);
+    // === configuration ===
 
-	ini_set('display_errors', 		DEBUG);
+    define('DEBUG', 0);
+    define('PORT_CHECK_TIMEOUT', 3);
+    ini_set('display_errors', DEBUG);
+    error_reporting(DEBUG ? E_ALL : 0);
 
-	error_reporting(DEBUG ? E_ALL : 0);
+    header('Content-type: text/plain');
 
-	header('Content-type: text/plain');
+    // === functions ===
 
-	// === functions ===
+    function check_port($ip, $port)
+    {
+        return @fsockopen($ip, $port, $errno, $errstr, PORT_CHECK_TIMEOUT);
+    }
 
-	function check_port($ip, $port)
-	{
-		return @fsockopen($ip, $port, $errno, $errstr, PORT_CHECK_TIMEOUT);
-	}
+    function updatedbinfo($gameinfo) {
+        global $db;
 
-	function updatedbinfo($gameinfo) {
-		global $db;
-		$fields = array_keys($gameinfo);
+        $fields = array_keys($gameinfo);
+        $query  = $db->prepare('INSERT OR ABORT INTO `servers` ('.implode(', ', $fields).')
+                                VALUES (:'.implode(', :', $fields).')
+        ');
+        $result = $query->execute($gameinfo);
+        if (!$result) {
+            $query  = $db->prepare('UPDATE OR FAIL `servers` SET '.implode('=?, ', $fields).'=? WHERE address = :address');
+            $result = $query->execute(array_merge(array_values($gameinfo), array(':address' => $gameinfo['address'])));
+        }
 
-		$query  = $db->prepare('INSERT OR ABORT INTO `servers` ('.implode(', ', $fields).') VALUES (:'.implode(', :', $fields).')');
-		$result = $query->execute($gameinfo);
-		if (!$result) {
-			$query  = $db->prepare('UPDATE OR FAIL `servers` SET '.implode('=?, ', $fields).'=? WHERE address = :address');
-			$result = $query->execute(array_merge(array_values($gameinfo), array(':address' => $gameinfo['address'])));
-		}
+        if (DEBUG) $query->debugDumpParams();
 
-		if (DEBUG) $query->debugDumpParams();
-		return $result;
-	}
+        if (!isset($_REQUEST['clients']) || $_REQUEST['clients'] == "")
+            return true;
 
-	// === body ===
+        $query = $db->prepare('DELETE FROM clients WHERE address = :addr');
+        $query->bindValue(':addr', $gameinfo['address'], PDO::PARAM_STR);
+        $query->execute();
+        
+        $clients = explode(",", $_REQUEST['clients']);
+        foreach ($clients as $client)
+        {
+            $query = $db->prepare("INSERT INTO clients ('address','client','ts')
+                                VALUES (:addr, :client, :ts)");
+            $query->bindValue(':addr', $gameinfo['address'], PDO::PARAM_STR);
+            $query->bindValue(':client', base64_decode($client), PDO::PARAM_STR);
+            $query->bindValue(':ts', time(), PDO::PARAM_INT);
+            $query->execute();
+        }
+        return true;
+    }
 
-	// make sure everything required is actually set.
-	foreach(array('port', 'name', 'state', 'map', 'mods', 'players') as $key)
-		if(!isset($_REQUEST[$key]))
-			die('field "'.$key.'" is not set');
+    // === body ===
 
-	try 
-	{
-		$db = new PDO('sqlite:db/openra.db');
+    // make sure everything required is actually set.
+    foreach(array('port', 'name', 'state', 'map', 'mods', 'players') as $key)
+        if(!isset($_REQUEST[$key]))
+            die('field "'.$key.'" is not set');
 
-		$ip   = $_SERVER['REMOTE_ADDR'];
-		$port = $_REQUEST['port'];
-		$addr = $ip.':'.$port;
-		$name = urldecode($_REQUEST['name']);
-		
-		if ($_REQUEST['state'] == 3)
-		{
-			$remove = $db->prepare('DELETE FROM `servers` WHERE address = :addr');
-			$remove->bindValue(':addr', $addr, PDO::PARAM_STR);
-			$remove->execute();
-			unset($db);
-			$file = 'games.log';
-			$data = $addr.",".$_REQUEST['map'].",".$_REQUEST['mods']."\n";
-			file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
-			exit;
-		}
+    try 
+    {
+        $db = new PDO('sqlite:db/openra.db');
 
-		if (isset($_REQUEST['new']))
-		{
-			if(!check_port($ip, $port))
-				$name = '[down]' . $name;
+        $ip   = $_SERVER['REMOTE_ADDR'];
+        $port = $_REQUEST['port'];
+        $addr = $ip.':'.$port;
+        $name = urldecode($_REQUEST['name']);
+        $started = '';
 
-			$games = file_get_contents("games.txt");
-			file_put_contents("games.txt", $games + 1);
-		}
+        $version_arr = explode('@', $_REQUEST['mods']);
+        $game_mod = array_shift($version_arr);
+        $version = implode('@', $version_arr);
+        
+        if ($_REQUEST['state'] == 2)
+        {
+            $query = $db->prepare('SELECT * FROM servers WHERE address = :addr');
+            $query->bindValue(':addr', $addr, PDO::PARAM_STR);
+            $query->execute();
+            $result = $query->fetchAll();
+            foreach ( $result as $row )
+            {
+                if ($row['state'] == 1)
+                {
+                    $started = date('Y-m-d H:i:s');
 
-		updatedbinfo(
-			array(
-				'name'		=> $name,
-				'address'	=> $addr,
-				'players'	=> $_REQUEST['players'],
-				'state'		=> $_REQUEST['state'],
-				'ts'		=> time(),
-				'map'		=> $_REQUEST['map'], 
-				'mods'		=> $_REQUEST['mods'],
-			)
-		);
-		unset($db);
-	}
-	catch (PDOException $e)
-	{
-		die($e->getMessage());
-	}
+                    $insert = $db->prepare("INSERT INTO started ('game_id','name','address','map','game_mod','version','protected','started','players','spectators','bots')
+                            VALUES (:game_id, :name, :address, :map, :game_mod, :version, :protected, :started, :players, :spectators, :bots)"
+                    );
+                    $insert->bindValue(':game_id', $row['id'], PDO::PARAM_INT);
+                    $insert->bindValue(':name', $_REQUEST['name'], PDO::PARAM_STR);
+                    $insert->bindValue(':address', $addr, PDO::PARAM_STR);
+                    $insert->bindVAlue(':map', $_REQUEST['map'], PDO::PARAM_STR);
+                    $insert->bindValue(':game_mod', $game_mod, PDO::PARAM_STR);
+                    $insert->bindValue(':version', $version, PDO::PARAM_STR);
+                    $insert->bindValue(':protected', isset($_REQUEST['protected']) ? $_REQUEST['protected'] : 0, PDO::PARAM_STR);
+                    $insert->bindValue(':started', $started, PDO::PARAM_STR);
+                    $insert->bindValue(':players', $_REQUEST['players'], PDO::PARAM_INT);
+                    $insert->bindValue(':spectators', isset($_REQUEST['spectators']) ? $_REQUEST['spectators'] : 0, PDO::PARAM_INT);
+                    $insert->bindValue(':bots', isset($_REQUEST['bots']) ? $_REQUEST['bots'] : 0, PDO::PARAM_INT);
+                    $insert->execute();
+                    if (DEBUG) $insert->debugDumpParams();
+                }
+                else
+                    $started = $row['started'];
+                break;
+            }
+        }
+
+        if ($_REQUEST['state'] == 3)
+        {
+            $query = $db->prepare('SELECT id,started FROM servers WHERE address = :addr');
+            $query->bindValue(':addr', $addr, PDO::PARAM_STR);
+            $query->execute();
+            $result = $query->fetchAll();
+            foreach ( $result as $row )
+            {
+                if ($row['started'] == '')
+                    break;
+                $insert = $db->prepare("INSERT INTO finished ('game_id','name','address','map','game_mod','version','protected','started','finished')
+                            VALUES (:game_id, :name, :address, :map, :game_mod, :version, :protected, :started, :finished)"
+                );
+                $insert->bindValue(':game_id', $row['id'], PDO::PARAM_INT);
+                $insert->bindValue(':name', $_REQUEST['name'], PDO::PARAM_STR);
+                $insert->bindValue(':address', $addr, PDO::PARAM_STR);
+                $insert->bindVAlue(':map', $_REQUEST['map'], PDO::PARAM_STR);
+                $insert->bindValue(':game_mod', $game_mod, PDO::PARAM_STR);
+                $insert->bindValue(':version', $version, PDO::PARAM_STR);
+                $insert->bindValue(':protected', isset($_REQUEST['protected']) ? $_REQUEST['protected'] : 0, PDO::PARAM_STR);
+                $insert->bindValue(':started', $row['started'], PDO::PARAM_STR);
+                $insert->bindValue(':finished', date('Y-m-d H:i:s'), PDO::PARAM_STR);
+                $insert->execute();
+                if (DEBUG) $insert->debugDumpParams();
+
+                $played_counter = 1;
+                $query = $db->prepare('SELECT played_counter FROM map_stats WHERE map = :map');
+                $query->bindValue(':map', $_REQUEST['map'], PDO::PARAM_STR);
+                $query->execute();
+                $result = $query->fetchAll();
+                if ($result)
+                {
+                    foreach ($result as $row)
+                    {
+                        $played_counter = $row['played_counter'] + 1;
+                        break;
+                    }
+                    $insert = $db->prepare('UPDATE map_stats SET played_counter = :played_counter, last_change = :last_change WHERE map = :map');
+                }
+                else
+                {
+                    $insert = $db->prepare("INSERT INTO map_stats ('map','played_counter','last_change')
+                            VALUES (:map, :played_counter, :last_change)"
+                    );
+                }
+                $insert->bindValue(':map', $_REQUEST['map'], PDO::PARAM_STR);
+                $insert->bindValue(':played_counter', $played_counter, PDO::PARAM_INT);
+                $insert->bindValue(':last_change', date('Y-m-d H:i:s'), PDO::PARAM_STR);
+                $insert->execute();
+                if (DEBUG) $insert->debugDumpParams();
+                break;
+            }
+            $remove = $db->prepare('DELETE FROM `servers` WHERE address = :addr');
+            $remove->bindValue(':addr', $addr, PDO::PARAM_STR);
+            $remove->execute();
+            $db->query('DELETE FROM servers WHERE (' . time() . ' - ts > 300)');
+
+            if (isset($_REQUEST['clients']))
+            {
+                $remove = $db->prepare('DELETE FROM clients WHERE address = :addr OR (' . time() . ' - ts > 300)');
+                $remove->bindValue(':addr', $addr, PDO::PARAM_STR);
+                $remove->execute();
+            }
+            unset($db);
+            exit;
+        }
+
+        if (isset($_REQUEST['new']))
+        {
+            if(!check_port($ip, $port))
+                $name = '[down]' . $name;
+
+            $games = file_get_contents("games.txt");
+            file_put_contents("games.txt", $games + 1);
+        }
+
+        updatedbinfo(
+            array(
+                'name'      => $name,
+                'address'   => $addr,
+                'players'   => $_REQUEST['players'],
+                'state'     => $_REQUEST['state'],
+                'ts'        => time(),
+                'map'       => $_REQUEST['map'], 
+                'mods'      => $_REQUEST['mods'],
+                'bots'      => isset($_REQUEST['bots']) ? $_REQUEST['bots'] : 0,
+                'spectators'=> isset($_REQUEST['spectators']) ? $_REQUEST['spectators'] : 0,
+                'maxplayers'=> isset($_REQUEST['maxplayers']) ? $_REQUEST['maxplayers'] : 0,
+                'protected' => isset($_REQUEST['protected']) ? $_REQUEST['protected'] : 0,
+                'started'   => $started,
+            )
+        );
+        unset($db);
+    }
+    catch (PDOException $e)
+    {
+        die($e->getMessage());
+    }
 
 ?>
