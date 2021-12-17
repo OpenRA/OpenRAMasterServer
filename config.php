03dbfb0a61f91dde6a9c1f2cb9b62f75b638866f
Paul Chote <pchote@users.noreply.github.com>
2021-12-17 19:42:42 +0000

    Add input validation to mitigate previous attacks.

--- config.php
+++ config.php
@@ -6,5 +6,9 @@ define('DEBUG', 0);
 define('PORT_CHECK_TIMEOUT', 3);
 define('STALE_GAME_TIMEOUT', 300);
 define('ENABLE_GEOIP', true);
+define('MAX_PLAYERS_COUNT', 32);
+define('MAX_SPECTATORS_COUNT', 10);
+define('MAX_LEGACY_PLAYERS_COUNT', 16);
+define('MAX_LEGACY_BOTS_COUNT', 16);
 
 ?>
